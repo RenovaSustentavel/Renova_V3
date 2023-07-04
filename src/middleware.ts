dc7c68c47039dafc9a29ab82e5599980c0ae20c2
gusesba <gustavoesmanhotto@hotmail.com>
2023-07-04 20:54:40 -0300

    refactor: retirados alguns logs

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,23 +3,17 @@ import { verifyAuth } from "./lib/auth";
 
 export async function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith("/signin")) {
-    console.log("página signin");
     try {
       await verifyAuth(request);
-      console.log("página signin - usuário logado");
       return NextResponse.redirect(new URL("/", request.url));
     } catch (err) {
-      console.log("página signin - usuário não logado");
       return NextResponse.next();
     }
   } else {
     try {
-      console.log("página não signin");
       await verifyAuth(request);
-      console.log("página não signin - usuário logado");
       return NextResponse.next();
     } catch (err) {
-      console.log("página não signin - usuário não logado");
       return NextResponse.redirect(new URL("/signin", request.url));
     }
   }
